46663597b17639bf73a798204e5fa1b70de239e0
DoanQuynhChi3112 <doanthiquynhchi3112@gmail.com>
2023-12-23 23:12:31 +0700

    push

--- BTLNHOM01/Data/ApplicationDbcontext.cs
+++ BTLNHOM01/Data/ApplicationDbcontext.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using BTLNHOM01.Data;
 using BTLNHOM01.Models;
-using BTLNHOM01.Models;
 
 namespace BTLNHOM01.Data
 {
@@ -12,7 +11,6 @@ namespace BTLNHOM01.Data
         public DbSet<BTLNHOM01.Models.DanhMucHang> DanhMucHang { get; set; } = default!;
         public DbSet<BTLNHOM01.Models.DonHang> DonHang { get; set; } = default!;
         public DbSet<BTLNHOM01.Models.KhachHang> KhachHang { get; set; } = default!;
-        public DbSet<BTLNHOM01.Models.Account> Account { get; set; } = default!;
         public DbSet<BTLNHOM01.Models.NhanVien> NhanVien { get; set; } = default!;
         public DbSet<BTLNHOM01.Models.PhieuNhap> PhieuNhap { get; set; } = default!;
         public DbSet<BTLNHOM01.Models.phieuxuat> phieuxuat { get; set; } = default!;
---
+++ BTLNHOM01/Migrations/20231223155343_Create_table_DanhMucHang.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BTLNHOM01.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_table_DanhMucHang : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    PassWord = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.UserName);
+                });
+        }
+    }
+}
